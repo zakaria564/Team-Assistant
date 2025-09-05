d2a1d4ed4c9bc2f537792cd655916aa6e850f139
Zakaria Boukhari <taxizak81@gmail.com>
2025-09-05 22:16:41 +0000

    https://1drv.ms/i/c/eae477c7a7dfde90/EXeHQI-DcA1Jk_UQpxi0fXMBUIBJDmDVgL1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,8 @@ export default function RootLayout({
   return (
     <html lang="fr" suppressHydrationWarning>
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/icon.png" type="image/png" sizes="any" />
+        <link rel="apple-touch-icon" href="/icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
