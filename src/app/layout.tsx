4198a752a61524944982bc4c13a3d3147d9c1f56
Zakaria Boukhari <taxizak81@gmail.com>
2025-09-05 22:39:40 +0000

    D'après les images que vous avez partagées, je vois un reçu de paiement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   return (
     <html lang="fr" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/icon.png" type="image/png" sizes="any" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/football-logo-32.png" type="image/png" sizes="any" />
+        <link rel="apple-touch-icon" href="/football-logo-192.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
