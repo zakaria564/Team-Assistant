e16b65b08b607033a76b1913bcee5bb8b1e2faee
Zakaria Boukhari <taxizak81@gmail.com>
2025-12-10 23:01:45 +0000

    non ce n'est pas ça tu dois modifier cette modification que tu viens de

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
             </p>
           </div>
         </section>
-        <section id="features" className="container space-y-6 bg-muted py-8 md:py-12 lg:py-24">
+        <section id="features" className="container space-y-6 py-8 md:py-12 lg:py-24">
           <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
             <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-5xl">
               Fonctionnalités
